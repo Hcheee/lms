5a7115ad3a38f20d00efb6f00bf35d8b1b47e6fd
HanhChi <23a1001d0047@students.hou.edu.vn>
2026-09-09 13:10:28 +0700

    feat: them ket noi PDO

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+
+$config = require __DIR__ . '/config.php';
+
+$dsn = "mysql:host={$config['db_host']};dbname={$config['db_name']};charset={$config['db_charset']}";
+
+$pdo = new PDO(
+    $dsn,
+    $config['db_user'],
+    $config['db_pass'],
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]
+);
